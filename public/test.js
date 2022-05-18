8a056eddbd4abbc46b0a1bc4b9562729e61a4f94
nemetviiktor <dajlefromcb@gmail.com>
2022-05-18 11:52:29 +0200

    admin page

--- public/test.js
+++ public/test.js
@@ -1,51 +1,87 @@
 var canvas = document.getElementById('canvas1');
 canvas.width = 600;
 canvas.height = 400;
-
-
 var ctx = canvas.getContext('2d');
-var bullets =[];
 
-class Bullet{
-  constructor(x,y){
-      this.x = x;
-      this.y = y;
-      this.height = 20;
-      this.width = 3;
-      this.id = socket.id;
-     
-  }
-  move(){
-      this.y -= 5;
+//socket.emit("startGame", {width : canvas.width, height: canvas.height});
+
+var background = new Image();
+background.src = "grass.jpeg";
+
+background.onload = function () {
+  ctx.drawImage(background, 0, 0);
+}
+
+class Obstackle {
+
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.height = 10;
+    this.width = canvas.width;
+
   }
 
-  draw(ctx){
-    ctx.fillStyle = "red";
-    
+  draw(ctx) {
+    ctx.fillStyle = "black";
+
     ctx.fillRect(this.x, this.y, this.width, this.height);
-    
+
+  }
+
+}
+
+var bullets = [];
+
+class Bullet {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.height = 20;
+    this.width = 3;
+
+  }
+  move() {
+    this.y -= 5;
+  }
+
+  clear() {
+
+    var prevCoords = {};
+    prevCoords.x = Math.floor(this.x - 2);
+    prevCoords.y = Math.floor(this.y - 2);
+    ctx.clearRect(prevCoords.x, prevCoords.y, this.width + 2, this.height + 2);
+  };
+
+  draw(ctx) {
+    ctx.fillStyle = "red";
+
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+
   }
 
 }
 
 class Player {
-  constructor(x, y ,name) {
+  constructor(x, y, id) {
     this.x = x;
     this.y = y;
     this.width = 30;
     this.height = 30;
     this.speed = 4;
-    this.color = ctx.fillStyle ;
+    this.color = ctx.fillStyle;
     this.playerRounds = [];
     this.shootPressed = false;
-    this.name = name;
-   
+    this.isDead = false;
+    this.id = id;
+
+
     this.movement = {
       up: false,
       down: false,
       left: false,
       right: false,
-      
+
     };
     document.addEventListener("keydown", this.keydown);
     document.addEventListener("keyup", this.keyup);
@@ -55,30 +91,32 @@ class Player {
     this.clear();
     this.move();
     ctx.fillRect(this.x, this.y, this.width, this.height);
-  
+
   }
-  
-  shoot(){
-    bullets.forEach(bullet =>{
-     
-          bullet.draw(ctx); 
-     
+
+  shoot() {
+    g.playerRounds.forEach(bullet => {
+
+      bullet.draw(ctx);
+
     });
+
+
     /*rect1.x < rect2.x + rect2.w &&
     rect1.x + rect1.w > rect2.x &&
     rect1.y < rect2.y + rect2.h &&
     rect1.h + rect1.y > rect2.y*/
   }
-    
 
-  
-  clear () {
 
-      var prevCoords = {};
-      prevCoords.x = Math.floor(this.x - 2);
-      prevCoords.y = Math.floor(this.y - 2);
-      ctx.clearRect(prevCoords.x, prevCoords.y, this.width+2, this.height+2);
-      };
+
+  clear() {
+
+    var prevCoords = {};
+    prevCoords.x = Math.floor(this.x - 2);
+    prevCoords.y = Math.floor(this.y - 2);
+    ctx.clearRect(prevCoords.x, prevCoords.y, this.width + 2, this.height + 2);
+  };
 
   move() {
     if (this.downPressed) {
@@ -94,8 +132,8 @@ class Player {
     if (this.rightPressed) {
       this.x += this.speed;
     }
-      
- };
+
+  };
 
   keydown = (e) => {
     if (e.code === "ArrowUp") {
@@ -108,16 +146,15 @@ class Player {
       this.movement.left = true;
     }
     if (e.code === "ArrowRight") {
-      this.movement.right = true; 
-      
+      this.movement.right = true;
+
     }
     if (e.code === "Space") {
-      
+
       console.log("fireeeee");
       //socket.emit('addBullet', 1);
- 
-        socket.emit('addBullet',{x:this.x , y:this.y, w:3, h: 20,  id: 0, dir : 'up'});
-  
+
+      socket.emit('addBullet', { x: this.x, y: this.y, w: 3, h: 20, dir: 'up' });
     }
   };
 
@@ -137,58 +174,67 @@ class Player {
     if (e.code === "Space") {
       this.shootPressed = false;
       console.log("fire stopped");
-    //  socket.emit('addBullet', 0);
-    
+      //  socket.emit('addBullet', 0);
+
     }
   };
 }
 
-
 var socket = io();
 var color;
 
-var g = new Player(0,0,color, "plosd");
+var g = new Player(0, 0, color, socket.id);
+var obstackle = new Obstackle(0, canvas.height / 2)
 
-
-socket.on('new player', function(data){
+socket.on('new player', function (data) {
   g.x = data.x;
   g.y = data.y;
 
-  
 });
 
 
-setInterval(function() {
+setInterval(function () {
   socket.emit('movement', g.movement);
- 
+
 }, 1000 / 60);
 
-socket.on('bupdate',function(projectileData){
+socket.on('bupdate', function (projectileData) {
   //új lövedék vételekor kliens létrehoz egy újat
-  for(var i in projectileData){
-    bullets[i] = new Bullet(projectileData[i].x + 15, projectileData[i].y -40); 
-   
-   }
-  });
-  
+  //console.log(projectileData.id);
+  for (var i in projectileData) {
 
+    g.playerRounds[i] = new Bullet(projectileData[i].x + 15, projectileData[i].y);
 
-socket.on('state', function(players) {
-  //console.log(players);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-   
-    
-  for (var i in players) {
-  
-      g.x = players[i].x;
-      g.y = players[i].y;
-      
-
-      ctx.fillStyle = players[i].color;
-       g.draw(ctx); 
-       g.shoot();
   }
 });
+
+
+
+socket.on('state', function (players) {
+  //console.log(players);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  obstackle.draw(ctx);
+
+  for (var i in players) {
+
+    g.x = players[i].x;
+    g.y = players[i].y;
+
+    ctx.fillStyle = players[i].color;
+
+
+    g.draw(ctx);
+    g.shoot();
+
+
+
+
+  }
+
+
+});
+
+
 
 var messages = document.getElementById('messages');
 var form = document.getElementById('form');
@@ -196,52 +242,65 @@ var input = document.getElementById('input');
 var nameInput = document.getElementById('nameInput');
 var startButton = document.getElementById('startButton');
 var startDiv = document.getElementById('startDiv');
-    //var nameForm = document.getElementById('name').value;
-
-socket.on('gameState', function(data){
-    
-
-});
+var newRow = document.getElementById('score_board').insertRow();
+//var nameForm = document.getElementById('name').value;
 
 
 startButton.addEventListener('click', () => {
-  if (nameInput.value == 0 || nameInput.value == undefined){
+  if (nameInput.value == 0 || nameInput.value == undefined) {
     alert("Name is required to play");
 
-  }else{
-    socket.emit('new player', {name : nameInput.value});
-    socket.emit('chatter ', {name : nameInput.value});
+  } else {
+    socket.emit('new player', { name: nameInput.value });
+    socket.emit('chatter ', { name: nameInput.value });
     socket.emit('startGame');
     //startButton.style.display = "none";
-    canvas.style.display ="block";
-    startDiv.style.display ="none";
-    form.style.display ="block";
+    canvas.style.display = "block";
+    startDiv.style.display = "none";
+    form.style.display = "block";
   }
 });
-    form.addEventListener('submit', function(e) {
-         e.preventDefault();
-         
-         if (input.value) {
-             socket.emit('chatter', input.value);
-            
-            input.value = '';
-         }
-    });
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
 
-    socket.on('playerName', function(data) {
-      g.name = data.name;
-});
-    socket.on('admin', function(msg) {
-        var item = document.createElement('p');
-        item.textContent =   msg  ;
-        messages.appendChild(item);
-        window.scrollTo(0, document.body.scrollHeight);
-  });
-  socket.on('chatter', function(msg) {
-    var item = document.createElement('p');
-    item.textContent =  msg.name+" : "+ msg.msg  ;
-    console.log(g.name);
-    messages.appendChild(item);
-    window.scrollTo(0, document.body.scrollHeight);
+  if (input.value) {
+    socket.emit('chatter', input.value);
+
+    input.value = '';
+  }
 });
 
+socket.on('playerName', function (data) {
+  g.name = data.name;
+});
+socket.on('admin', function (msg) {
+  var item = document.createElement('p');
+  item.textContent = msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+socket.on('chatter', function (msg) {
+  var item = document.createElement('p');
+  item.textContent = msg.name + " : " + msg.msg;
+  //console.log(g.name);
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+//új rekord a html táblába
+socket.on("table", function (data) {
+  var newCell = newRow.insertCell();
+  var newText = document.createTextNode(data.name + "" + data.score);
+  newRow.appendChild(newText);
+
+});
+
+socket.on('redirect', function (destination) {
+  alert("The admin has restarted the game");
+  window.location.href = destination;
+});
+
+socket.on('killed', function (destination) {
+  //alert("You died!");
+  window.location.href = destination;
+});
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+const { Socket } = require('engine.io');
 const express = require('express');
 const app = express();
 const http = require('http');
+const { SocketAddress } = require('net');
 const { join } = require('path');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+const { SocksClient } = require('socks');
+const { PassThrough } = require('stream');
 const io = new Server(server);
 const sqlite3 = require('sqlite3').verbose();
 
@@ -15,9 +19,6 @@ const db = new sqlite3.Database('./mock.db', sqlite3.OPEN_READWRITE, (err) => {
 db.run('DELETE FROM players');
 
 //db.run('CREATE TABLE players(id, name, score)');
-//db.run('CREATE TABLE users(first_name, last_name, username, password, email, id)');
-
-
 
 app.use(express.static('public'));
 
@@ -26,29 +27,30 @@ app.get('/', function (req, res) {
 });
 
 app.get('/admin', function (req, res) {
-  res.sendFile(__dirname + '/admin.html');
-}
+  res.sendFile(__dirname + '/public/admin.html');
+});
 
 var playersTop = [];
 var playersBottom = [];
-var gamers = [];
 var currentClients;
 var players = {};
 var bullet_array = [];
-var shootDir;
-var shootingDir;
-var enemy;
-var playersocket;
+var num = 0;
+var score = 0;
+const shoot_speed = 5;
+
+
+
 
 io.on('connection', (socket) => {
-  playersocket = socket.id;
   socket.on('chatter', (msg) => {
     io.emit('chatter', { msg: msg, name: players[socket.id].name });
   });
 
+
   socket.on('startGame', () => {
     if (currentClients < 2) {
-      io.emit('chatter', "waiting for other players");
+      io.emit('admin', "@Admin : waiting for other players");
     } else {
       io.emit('startGame');
       io.emit('admin', "@Admin : The game has started");
@@ -57,6 +59,8 @@ io.on('connection', (socket) => {
 
   const colorString = "#" + Math.floor(Math.random() * 16777215).toString(16);
   socket.on('new player', function (data) {
+    num++;
+    console.log("játékosok száma : " + num);
     console.log(socket.id);
     io.emit('admin', "@Admin : " + data.name + " has joined!");
 
@@ -65,7 +69,7 @@ io.on('connection', (socket) => {
     currentClients = io.engine.clientsCount;
     console.log("number of clients : ", currentClients);
 
-    gamers.push(players[socket.id]);
+
 
     players[socket.id] = {
       x: 300,
@@ -76,18 +80,22 @@ io.on('connection', (socket) => {
       id: socket.id,
       name: "playerX", //unidentified lenne data.name-el
       bullets: [],
+      number: num,
       //id : socket.id,
       color: colorString,
-    }
+      kills: score,
+      hp: 3
 
+    }
+    id = socket.id;
 
     players[socket.id].name = data.name;
-    console.log(gamers.length);
-    //gamers.push(players[socket.id]); 
+
+
     console.log(" player " + data.name + "created in : ", players[socket.id].x, players[socket.id].y)
 
     let sql = 'INSERT INTO players (id, name, score) VALUES(?,?,?)';
-    db.run(sql, [socket.id, data.name, 0], (err) => {
+    db.run(sql, [socket.id, data.name, players[socket.id].kills], (err) => {
       if (err) return console.error(err.message);
 
       console.log("A new row has been created");
@@ -100,37 +108,41 @@ io.on('connection', (socket) => {
 
       rows.forEach((row) => {
         console.log(row);
+        io.emit('table', { name: row.name, score: row.score });
       })
     })
 
 
     for (i in players) {
 
-      if (currentClients % 2 == 0) {
+      if (players[socket.id].number % 2 == 0) {
 
-        players[socket.id].x = 0;
-        players[socket.id].y = 0;
-        // playersBottom.push(players[socket.id]);
-
+        players[socket.id].x = 1;
+        players[socket.id].y = 10;
+        playersBottom.push(players[socket.id]);
 
 
       } else {
         players[socket.id].x = 600;
         players[socket.id].y = 400;
 
-        //playersTop.push(players[socket.id]);
+        playersTop.push(players[socket.id]);
 
       }
     }
 
-    socket.emit('new player', { x: players[socket.id].x, y: players[socket.id].y, name: players[socket.id].name });
+    socket.emit('new player', players);
     console.log("lent " + playersBottom.length);
     console.log("fent" + playersTop.length);
   });
 
+
+
   socket.on('movement', function (data) {
+    //ha players[socket.id] = undefined akkor üres dictet hoz létre.
     var player = players[socket.id] || {};
     if (currentClients < 2) {
+
       player.x -= 0;
       player.y -= 0;
     } else {
@@ -159,97 +171,139 @@ io.on('connection', (socket) => {
 
     }
 
+    // Közép vonal akadály detektálós feltétel vizsgálat.
+    if (player.x < 0 + 600 &&
+      player.x + player.w > 0 &&
+      player.y < 200 + 10 &&
+      player.y + player.h > 200) {
+      if (player.y < 200) {
+        player.y = 200 - player.h;
+      } if (player.y > 200) {
+        player.y = 190 + player.h;
+      }
+    }
+
   });
 
   socket.on('addBullet', function (data) {
     if (players[socket.id] == undefined || currentClients < 2) return;    //ha nincs meg a min2 kliens akkor addig ne lőjön.
     data.y = players[socket.id].y;
-    data.x = players[socket.id].x
-    if (players[socket.id].y > 200) {
-      data.dir = 'up';
+    data.x = players[socket.id].x;
+    data.owner_id = socket.id;
+    data.owner_name = players[socket.id].name;
+
+    if (players[socket.id].number % 2 == 0) {
+      players[socket.id].shoot = "up";
+
+      data.y = players[socket.id].y + 30;
 
     } else {
-      data.dir = 'down';
-    }
-    shootDir = data.dir;
-    bullet_array.push(data);
 
+      players[socket.id].shoot = "down";
+      data.y = players[socket.id].y - 30;
+
+    }
+    data.dir = players[socket.id].shoot
+    shootDir = players[socket.id].shoot;
+    bullet_array.push(data);
     //players[socket.id].bullets.push(data);
+    //bullets = players[socket.id].bullets;
+
 
   });
 
   socket.on('disconnect', () => {
     // if(players[socket.id].name == undefined) return;
     io.emit('admin', "@Admin :", "A user has disconnected!");
-    for (i in gamers) {
-      gamers.splice(i, 1);
-    }
+
     delete players[socket.id];
 
 
   });
+
+  socket.on('restart', function () {
+
+    io.emit('admin', "The admin has restarted the game");
+    for (var i in players)
+
+      delete players[i];
+
+
+    var destination = '/index.html';
+    io.emit('redirect', destination);
+
+  });
+
 });
 
 
 function updateBullet() {
 
-  var count = 0;
   for (i in bullet_array) {
 
     bullet = bullet_array[i];
-    if (shootDir == 'up') {
-      bullet.y -= 5;
+
+    if (bullet.dir == "up") {
+      bullet.y += shoot_speed;
+    } else if (bullet.dir == "down") {
+      bullet.y -= shoot_speed;
     }
+    //kimegy akkor törlődik. (bugos)
+    /*if ( bullet.y > 400 || bullet.y < 0 ) {
+      bullet_array.splice(i, 1);
+      i--;
+    }*/
 
-    if (shootDir == 'down') {
-      bullet.y += 5;
-    }
+    for (id in players) {
+      var player = players[id] || {};
 
+      // console.log(Object.keys(players).length);   
+      if (bullet.owner_id != id) {
 
-    for (j in players) {
-      //console.log("length of dict "+ Object.keys(players).length);
+        if (bullet.x < player.x + player.w &&
+          bullet.x + bullet.w > player.x &&
+          bullet.y < player.y + player.h &&
+          bullet.y + bullet.h > player.y) {
 
-      if (shootDir == 'up') {
-        if (bullet.x + 15 < players[j].x + players[j].w &&
-          bullet.x + 15 + bullet.w > players[j].x &&
-          bullet.y - 40 < players[j].y + players[j].h &&
-          bullet.y - 40 + bullet.h > players[j].y) {
+          player.hit = true;
 
+          var destination = '/index.html';
+          io.to(player.id).emit('killed', destination);
 
-          //console.log(this.name);
-          console.log("" + players[j].name + " was hit",);
-          io.emit('admin', "@Admin " + players[j].name + "was hit");
-          count++;
-          console.log(count);
+          console.log("" + player.name + " was hit by " + bullet.owner_name);
+          player.kills++;
+          io.emit('admin', "@Admin " + player.name + "was hit");
+          let sql = 'UPDATE players SET score=? WHERE name=?';
+          db.run(sql, [player.kills, bullet.owner_name], (err) => {
+            if (err) return console.error(err.message);
 
-          delete players[j];
+            console.log("Score is updated");
+          });
+
+          sql = 'SELECT * FROM players';
+
+          db.all(sql, [], (err, rows) => {
+            if (err) return console.error(err.message);
+
+            rows.forEach((row) => {
+              console.log(row);
+              io.emit('table', { name: row.name, score: row.score });
+            })
+          })
+
+          //score++;
+          //player.hp--;
+        }
+
+        if (player.hp <= 0) {
+          //delete players[id];
         }
       }
-
-      if (shootDir == 'down') {
-        if (bullet.x + 15 < players[j].x + players[j].w &&
-          bullet.x + 15 + bullet.w > players[j].x &&
-          bullet.y < players[j].y + players[j].h &&
-          bullet.y + bullet.h > players[j].y) {
-
-
-          //console.log(this.name);
-          console.log("" + players[j].name + " was hit",);
-          io.emit('admin', "@Admin " + players[j].name + "was hit");
-          count++;
-          console.log(count);
-
-          //delete players[j];
-        }
-      }
-
     }
   }
-
-
   io.emit('bupdate', bullet_array);
-}
 
+}
 setInterval(updateBullet, 1000 / 60);
 
 setInterval(function () {
@@ -260,4 +314,3 @@ setInterval(function () {
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
-
